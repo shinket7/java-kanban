edce1f538e459b43bc41bb46e7d94624d470805b
shinket7 <a.vitalik20@yandex.ru>
2025-06-07 13:14:18 +0300

    Add `fromString()`.
    Add `Task fromString(String)` to `FileBackedTaskManager`.

--- src/tracker/controllers/FileBackedTaskManager.java
+++ src/tracker/controllers/FileBackedTaskManager.java
@@ -1,12 +1,14 @@
 package tracker.controllers;
 
+import tracker.model.Epic;
 import tracker.model.Subtask;
 import tracker.model.Task;
+import tracker.model.TaskStatus;
 import tracker.model.TaskType;
 
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
 
 public class FileBackedTaskManager extends InMemoryTaskManager {
 
@@ -15,21 +17,57 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
     }
 
 //    public void test() {
-//        try {
-//            Files.createFile(Paths.get("ASD.txt"));
+//        try (FileWriter writer = new FileWriter("TaskManagerSave.txt", StandardCharsets.UTF_8)) {
+//            writer.write("первая строчка\n");
+//            writer.write("вторая строчка");
 //        } catch (IOException e) {
 //            System.out.println("wrong....");
 //        }
 //    }
 
-    public String toString(Task task) {
+    public static String toString(Task task) {
         final TaskType taskType = task.getTaskType();
-        String epicIdText = "";
+        final String epicIdText;
         if (taskType == TaskType.SUBTASK) {
-                final Subtask subtask = (Subtask) task;
-                epicIdText += subtask.getEpicId();
+            final Subtask subtask = (Subtask) task;
+            epicIdText = String.valueOf(subtask.getEpicId());
+        } else {
+            epicIdText = "";
         }
         return String.format("%d,%s,%s,%s,%s,%s", task.getTaskId(), task.getTaskType(), task.getSummary(),
                 task.getStatus(), task.getDescription(), epicIdText);
+    }
+
+    public static Task fromString(String value) {
+        String[] parts = value.split(",");
+        final int taskId = Integer.parseInt(parts[0]);
+        final String taskTypeText = parts[1];
+        final String summary = parts[2];
+        final String taskStatusText = parts[3];
+        final String description = parts[4];
+
+        final TaskStatus taskStatus;
+        if (taskStatusText.equals("IN_PROGRESS")) {
+            taskStatus = TaskStatus.IN_PROGRESS;
+        } else if (taskStatusText.equals("DONE")) {
+            taskStatus = TaskStatus.DONE;
+        } else {
+            taskStatus = TaskStatus.NEW;
+        }
+
+        final Task result;
+        if (taskTypeText.equals("EPIC")) {
+            result = new Epic(summary, description);
+        } else if (taskTypeText.equals("SUBTASK")) {
+            final int epicId = Integer.parseInt(parts[5]);
+            final Subtask subtask = new Subtask(summary, description);
+            subtask.setEpicId(epicId);
+            result = subtask;
+        } else {
+            result = new Task(summary, description);
+        }
+        result.setTaskId(taskId);
+        result.setStatus(taskStatus);
+        return result;
     }
 }
