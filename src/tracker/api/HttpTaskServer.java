69518ad8767b0cea4ee5e10269f1c211c431019d
shinket7 <a.vitalik20@yandex.ru>
2025-08-31 14:10:07 +0300

    Create `HttpTaskServer` without handlers.

---
+++ src/tracker/api/HttpTaskServer.java
@@ -0,0 +1,37 @@
+package tracker.api;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class HttpTaskServer {
+    private HttpServer server;
+
+    public static void main(String[] args) {
+        final HttpTaskServer taskServer = new HttpTaskServer();
+        taskServer.start();
+        taskServer.stop();
+    }
+
+    public void start() {
+        try {
+            server = HttpServer.create(new InetSocketAddress(8080), 0);
+        } catch (IOException e) {
+            System.out.println("Server start error");
+            return;
+        }
+        server.createContext("/tasks");
+        server.createContext("/subtasks");
+        server.createContext("/epics");
+        server.createContext("/history");
+        server.createContext("/prioritized");
+        server.start();
+    }
+
+    public void stop() {
+        if (server != null) {
+            server.stop(2);
+        }
+    }
+}
