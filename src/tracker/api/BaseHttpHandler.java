3351455bba1fc01d83134b265bc0bea3ff103702
shinket7 <a.vitalik20@yandex.ru>
2025-08-31 15:16:17 +0300

    Add task manager field to `BaseHttpHandler`.

--- src/tracker/api/BaseHttpHandler.java
+++ src/tracker/api/BaseHttpHandler.java
@@ -1,12 +1,19 @@
 package tracker.api;
 
 import com.sun.net.httpserver.HttpExchange;
+import tracker.controllers.TaskManager;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
 public class BaseHttpHandler {
+    final protected TaskManager taskManager;
+
+    public BaseHttpHandler(TaskManager taskManager) {
+        this.taskManager = taskManager;
+    }
+
     public void sendText(HttpExchange exchange, String text) throws IOException {
         byte[] resp = text.getBytes(StandardCharsets.UTF_8);
         exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
