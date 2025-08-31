42ee7a4b8b194f11acbb942a7de9476c5dbb34e3
shinket7 <a.vitalik20@yandex.ru>
2025-08-31 14:45:58 +0300

    Create `BaseHttpHandler`.
    Create methods: `sendText()`, `sendBadRequest()`, `sendNotFound()`, `sendNotAllowed()`, `sendHasOverlaps()`.

---
+++ src/tracker/api/BaseHttpHandler.java
@@ -0,0 +1,34 @@
+package tracker.api;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class BaseHttpHandler {
+    public void sendText(HttpExchange exchange, String text) throws IOException {
+        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+        exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+        exchange.sendResponseHeaders(200, resp.length);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(resp);
+        }
+    }
+
+    public void sendBadRequest(HttpExchange exchange) throws IOException {
+        exchange.sendResponseHeaders(400, 0);
+    }
+
+    public void sendNotFound(HttpExchange exchange) throws IOException {
+        exchange.sendResponseHeaders(404, 0);
+    }
+
+    public void sendNotAllowed(HttpExchange exchange) throws IOException {
+        exchange.sendResponseHeaders(405, 0);
+    }
+
+    public void sendHasOverlaps(HttpExchange exchange) throws IOException {
+        exchange.sendResponseHeaders(406, 0);
+    }
+}
